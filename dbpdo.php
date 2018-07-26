ff73e4c051b390e01ae82e908fce403797813ef4
kumike <michailkubrak@gmail.com>
2018-07-27 01:26:31 +0300

    обновлен файл dbpdo.php

--- dbpdo.php
+++ dbpdo.php
@@ -3,24 +3,23 @@
 $db_host = '127.0.0.1';
 $db_user = 'root';
 $db_pass = ',j,thrf';
-$db_name = 'iplogger';
-//$db_name = '';
+//$db_name = 'iplogger';
+
 #$db_name = 'ipbothoua';
+
+/*$db_host = '';*/
+//$db_user = '';
+//$db_pass = '';
+$db_name = '';
 
 try{
 //	$opt = [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC];
-	
 //	$dbh = new PDO("mysql:host=$db_host;dbname=$db_name;charset=utf8",$db_user,$db_pass,$opt);
+
 	$dbh = new PDO("mysql:host=$db_host;dbname=$db_name;charset=utf8",$db_user,$db_pass);
 //	$dbh->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
 	$dbh->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE,PDO::FETCH_ASSOC);
-//*** отключаем режим эмуляции, нужно для оперетора лимит, чтобы препаре не обрамлял кавычками значения подставленые
-//	$dbh->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
 	} catch (PDOException $e){
-		echo 'onoo!!!not work<br> '.$e->getMessage();
+		echo "<div class='hdb'>Настройте подключение к базе данных в файле dbpdo.php</div>\n</div>\n</div>\n</body>\n</html>";
 }
-
-
-
 ?>
-
